309909b34686ff052abc3663bc14722396035e88
anurajlad99 <86230633+anurajlad99@users.noreply.github.com>
2021-06-24 14:38:00 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int val = 0;
+        printf("\n Enter an ASCII value to get corresponding character = ");
+        scanf("%d",&val);
+
+        printf("\n\n character for given ASCII value is =<%d>.",val);
+        getch();
+        return 0;
+}
